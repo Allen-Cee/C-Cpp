7b8078e30c54adec77c99e4738aa5c8f50fb0ab6
SACxAoi <Aoi@Aois-MacBook-Pro.local>
2017-03-05 01:25:52 +0800

    17.03.05x01
    Add C&Cpp root directory
    Add the first program of Bailian

---
+++ Openjudge/1000 A+B Problem.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Cpp Project
+//
+//  Created by Aoi on 3/5/17.
+//  Copyright © 2017 Aoi. All rights reserved.
+//
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
