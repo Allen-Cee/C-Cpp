a05e602cf185411b91ef46e442b78e38985c908a
SACxAoi <Aoi@Aoi.local>
2017-03-29 11:08:43 +0800

    17.03.29x00
    Deleted /Desktop/cpp_tmp and started using /C&Cpp/Cpp_Project

---
+++ Cpp_Project/Cpp_Project/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Cpp_Project
+//
+//  Created by Aoi on 3/26/17.
+//  Copyright © 2017 Aoi. All rights reserved.
+//
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<memory>
+using namespace std;
+
+int main(){
+    //freopen("/Users/Aoi/Documents/Code/C&Cpp/Cpp_Project/Cpp_Project/data.txt","r",stdin);
+    
+    return 0;
+}
